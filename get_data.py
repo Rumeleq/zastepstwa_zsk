47ca76d45ce8d8a099c85e0fc8c96356bfbbd34e
Rumel <100099007+Rumeleq@users.noreply.github.com>
2026-05-20 21:10:52 +0200

    stary komit

---
+++ get_data.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+
+FILENAME = "Zastępstwa.html"
+
+
+def main():
+    with open(FILENAME, "r", encoding="utf-8") as f:
+        soup = BeautifulSoup(f.read(), "html.parser")
+    #print(soup)
+    rows = soup.find_all("tr")
+    data = [row.find_all("td") for row in rows]
+
+    del data[:2]
+
+    replacements = {}
+
+    for row in data:
+        lesson_info = [col.text for col in row]
+        teacher = lesson_info.pop(
+            5
+        ).strip()  # it takes 5th element (substitute teacher) of the list and then deletes it
+        if teacher not in replacements:
+            replacements[teacher] = [lesson_info]
+        else:
+            replacements[teacher].append(lesson_info)
+    return replacements
+
+
+def date():
+    with open(FILENAME, "r", encoding="utf-8") as f:  # Zastępstwa.html in main folder
+        soup = BeautifulSoup(f.read(), "html.parser")
+
+    # it looks for <h2>Okres: 26.11.2019 (wt.) - 26.11.2019 (wt.)</h2> and takes date from it
+    data = soup.find("h2")
+    date = str(data.text).split(" ")[1]
+
+    return date
+
+
+if __name__ == "__main__":
+    replacements = main()
+    print(replacements)
